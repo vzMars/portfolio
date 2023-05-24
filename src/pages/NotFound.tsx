2a8c266c387fc2d9cc3cac92fb67341d60cc143d
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-24 17:28:55 -0400

    all the pages of the portfolio

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <main>
+      <h1>NotFound</h1>
+    </main>
+  );
+};
+
+export default NotFound;
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <main>
+      <h1>Projects</h1>
+    </main>
+  );
+};
+
+export default Projects;
