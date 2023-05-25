9fe1ac0faf731ab5b83a640cba86f18d86fd6dd2
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-25 14:17:28 -0400

    mobile responsive home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 const Home = () => {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className='m-auto flex flex-col items-center justify-center py-8 text-center font-lexbold'>
+      <img
+        src='/images/headshot.jpg'
+        alt=''
+        className='mb-4 h-[196px] w-[196px] rounded-full object-cover sm:mb-8 sm:h-[320px] sm:w-[320px]'
+      />
+      <h1 className='mb-1 text-3xl text-sky-500 sm:text-5xl'>
+        Marcos Gonzalez
+      </h1>
+      <h2 className='text-2xl sm:text-4xl'>Software Engineer</h2>
     </main>
   );
 };
