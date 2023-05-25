f92f42aec884f1fb248a2d987b36e35842169236
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-05-25 13:56:12 -0400

    mobile responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,32 @@
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { HiMail } from 'react-icons/hi';
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className='mt-auto flex flex-col items-center justify-center py-4 text-xl sm:py-8 sm:text-2xl'>
+      <div className='flex space-x-5 py-2'>
+        <a
+          href='mailto:marcosgonzalez1731@gmail.com'
+          className='hover:text-sky-500'
+        >
+          <HiMail />
+        </a>
+        <a href='https://github.com/vzMars' className='hover:text-sky-500'>
+          <FaGithub />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/marcosgonzalez1731/'
+          className='hover:text-sky-500'
+        >
+          <FaLinkedin />
+        </a>
+        <a href='https://twitter.com/vzMars' className='hover:text-sky-500'>
+          <FaTwitter />
+        </a>
+      </div>
+      <p>&copy; {currentYear} • Marcos Gonzalez</p>
     </footer>
   );
 };
