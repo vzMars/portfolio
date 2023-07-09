db5c072325386da54697e464f6a688c2f0b1f4ed
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-07-09 13:10:08 -0400

    custom hook for allowing and blocking scroll on mobile devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
+import { useScrollBlock } from '../hooks/useScrollBlock';
 import Hamburger from 'hamburger-react';
 import Nav from './Nav';
 import MobileNav from './MobileNav';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [blockScroll, allowScroll] = useScrollBlock();
 
   const onToggle = () => {
     setOpen((status) => {
       if (status) {
-        document.body.style.overflow = '';
+        allowScroll();
       } else {
-        document.body.style.overflow = 'hidden';
+        blockScroll();
       }
 
       return !status;
