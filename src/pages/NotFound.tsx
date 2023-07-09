94c408f6acdffa913be7ec05b305cd9ba90e0991
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-07-08 23:08:41 -0400

    404 page added

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
-    <main>
-      <h1>NotFound</h1>
+    <main className='m-auto flex flex-col items-center justify-center py-8 text-center font-lexbold'>
+      <h1 className='text-7xl font-bold text-sky-500'>404</h1>
+      <p className='lead fw-normal pb-4 text-center font-medium'>
+        Oops! The page you are looking for does not exist.
+      </p>
+      <Link
+        to='/'
+        className='rounded-md bg-sky-500 px-4 py-3 text-xl font-bold'
+      >
+        Back To Home
+      </Link>
     </main>
   );
 };
